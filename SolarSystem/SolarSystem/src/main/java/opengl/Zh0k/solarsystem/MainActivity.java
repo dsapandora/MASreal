3d807f78f8784166eeb361c0c1c2259ef623b150
Ariel Vernaza <ariel.vernaza@admios-sa.com>
2014-04-12 22:42:46 -0500

    planets earth and sun working in the camara with opengl

--- SolarSystem/SolarSystem/src/main/java/opengl/Zh0k/solarsystem/MainActivity.java
+++ SolarSystem/SolarSystem/src/main/java/opengl/Zh0k/solarsystem/MainActivity.java
@@ -1,16 +1,30 @@
 package opengl.Zh0k.solarsystem;
 
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
+    private CamLayer mPreview;
+    static int counter=0;
+    private SolarSystemRenderer glView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        counter++;
+        if (counter==2) {
+            MediaPlayer mp=MediaPlayer.create(this, R.raw.track);
+            mp.start();
+        }
+/*
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         GLSurfaceView view = new GLSurfaceView(this);
@@ -18,7 +32,36 @@ public class MainActivity extends Activity {
         view.setEGLConfigChooser(8,8,8,8,16,0); //The new line
         view.setRenderer(new SolarSystemRenderer(true));
         setContentView(view);
+        */
     }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        final Window win = getWindow();
+        win.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        glView=new  SolarSystemRenderer(this,true);
+
+        mPreview = new CamLayer(this, glView);
+
+        setContentView(glView);
+        addContentView(mPreview, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+    }
+    protected void onPause() {
+        super.onPause();
+        if (counter>=2) {
+            System.exit(0);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- SolarSystem/SolarSystem/src/main/java/opengl/Zh0k/solarsystem/SolarSystemRenderer.java
+++ SolarSystem/SolarSystem/src/main/java/opengl/Zh0k/solarsystem/SolarSystemRenderer.java
@@ -2,18 +2,30 @@ package opengl.Zh0k.solarsystem;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+import android.view.SurfaceHolder;
+
 import java.lang.Math;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
-public class SolarSystemRenderer implements GLSurfaceView.Renderer {
+public class SolarSystemRenderer extends GLSurfaceView implements GLSurfaceView.Renderer, Camera.PreviewCallback,SurfaceHolder.Callback {
 
-    private boolean mTranslucentBackground;
+
     private Planet mPlanet;
     private float mTransY;
+    int onDrawFrameCounter=1;
+    FloatBuffer cubeBuff;
+    FloatBuffer texBuff;
 
+    int[] cameraTexture;
+    byte[] glCameraFrame=new byte[256*256]; //size of a texture must be a power of 2
     static float angle= 0.0f;
 
     public final static int X_VALUE= 0;
@@ -28,9 +40,15 @@ public class SolarSystemRenderer implements GLSurfaceView.Renderer {
     public final static int SS_FILLLIGHT1= GL10.GL_LIGHT1;
     public final static int SS_FILLLIGHT2= GL10.GL_LIGHT2;
 
-    public SolarSystemRenderer(boolean useTranslucentBackground)
+    public SolarSystemRenderer(Context c,boolean useTranslucentBackground)
     {
-        mTranslucentBackground = useTranslucentBackground;
+        super(c);
+        this.setEGLConfigChooser(5, 6, 5, 8, 16, 0);
+        super.setEGLConfigChooser(8 , 8, 8, 8, 16, 0);
+
+        this.setRenderer(this);
+        this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+
 
         m_Eyeposition[X_VALUE] = 0.0f;  //1
         m_Eyeposition[Y_VALUE] = 0.0f;
@@ -41,10 +59,13 @@ public class SolarSystemRenderer implements GLSurfaceView.Renderer {
 
         m_Sun= new Planet(50, 50,1.0f, 1.0f);  //4
         m_Sun.setPosition(0.0f, 0.0f, 0.0f);  //5
+
     }
 
     public void onDrawFrame(GL10 gl)
     {
+        onDrawFrameCounter++;
+
         float paleYellow[]={1.0f, 1.0f, 0.3f, 1.0f}; //1
         float white[]={1.0f, 1.0f, 1.0f, 1.0f};
         float cyan[]={0.0f, 1.0f, 1.0f, 1.0f};
@@ -52,9 +73,17 @@ public class SolarSystemRenderer implements GLSurfaceView.Renderer {
         float orbitalIncrement= 1.25f; //3
         float[] sunPos={0.0f, 0.0f, 0.0f, 1.0f};
 
-        gl.glEnable(GL10.GL_DEPTH_TEST);
+        gl.glEnable(GL10.GL_TEXTURE_2D);
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-        gl.glClearColor(0.0f,0.0f, 0.0f, 1.0f);
+
+        bindCameraTexture(gl);
+        gl.glLoadIdentity();
+        GLU.gluLookAt(gl, 0, 0, 4.2f, 0, 0, 0, 0, 1, 0);
+
+        //gl.glRotatef(onDrawFrameCounter,1,0,0); //Rotate the camera image
+         gl.glRotatef((float)Math.sin(onDrawFrameCounter/20.0f)*40,0,1,0); //Rotate the camera image
+         gl.glRotatef((float)Math.cos(onDrawFrameCounter/40.0f)*40,0,0,1); //Rotate the camera ima
+
         gl.glPushMatrix();   //4
         gl.glTranslatef(-m_Eyeposition[X_VALUE], -m_Eyeposition[Y_VALUE],-m_Eyeposition[Z_VALUE]);
 //5
@@ -107,14 +136,18 @@ public class SolarSystemRenderer implements GLSurfaceView.Renderer {
     }
 
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        gl.glDisable(GL10.GL_DITHER);
         gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
+
+        gl.glClearColor(0, 0, 0, 0);
         gl.glEnable(GL10.GL_CULL_FACE);
-        gl.glCullFace(GL10.GL_BACK);
         gl.glShadeModel(GL10.GL_SMOOTH);
         gl.glEnable(GL10.GL_DEPTH_TEST);
-        //gl.glDepthMask(false);
-        //initGeometry(gl);
+        cubeBuff = makeFloatBuffer(camObjCoord);
+        texBuff = makeFloatBuffer(camTexCoords);
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, cubeBuff);
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, texBuff);
+        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
         initLighting(gl);
     }
 
@@ -158,13 +191,115 @@ public class SolarSystemRenderer implements GLSurfaceView.Renderer {
         gl.glLoadIdentity();
     }
 
-    protected static FloatBuffer makeFloatBuffer(float[] arr)
-    {
-        ByteBuffer bb = ByteBuffer.allocateDirect(arr.length*4);
+    /**
+     * Generates a texture from the black and white array filled by the onPreviewFrame
+     * method.
+     */
+    void bindCameraTexture(GL10 gl) {
+        synchronized(this) {
+            if (cameraTexture==null)
+                cameraTexture=new int[1];
+            else
+                gl.glDeleteTextures(1, cameraTexture, 0);
+
+            gl.glGenTextures(1, cameraTexture, 0);
+            int tex = cameraTexture[0];
+            gl.glBindTexture(GL10.GL_TEXTURE_2D, tex);
+            gl.glTexImage2D(GL10.GL_TEXTURE_2D, 0, GL10.GL_LUMINANCE, 256, 256, 0, GL10.GL_LUMINANCE, GL10.GL_UNSIGNED_BYTE, ByteBuffer.wrap(glCameraFrame));
+            gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+        }
+    }
+
+    /**
+     * This method is called if a new image from the camera arrived. The camera
+     * delivers images in a yuv color format. It is converted to a black and white
+     * image with a size of 256x256 pixels (only a fraction of the resulting image
+     * is used). Afterwards Rendering the frame (in the main loop thread) is started by
+     * setting the newFrameLock to true.
+     */
+    public void onPreviewFrame(byte[] yuvs, Camera camera) {
+        int bwCounter=0;
+        int yuvsCounter=0;
+        for (int y=0;y<160;y++) {
+            System.arraycopy(yuvs, yuvsCounter, glCameraFrame, bwCounter, 240);
+            yuvsCounter=yuvsCounter+240;
+            bwCounter=bwCounter+256;
+        }
+    }
+
+    FloatBuffer makeFloatBuffer(float[] arr) {
+        ByteBuffer bb = ByteBuffer.allocateDirect(arr.length * 4);
         bb.order(ByteOrder.nativeOrder());
         FloatBuffer fb = bb.asFloatBuffer();
         fb.put(arr);
         fb.position(0);
         return fb;
     }
+
+    final static float camObjCoord[] = new float[] {
+            // FRONT
+            -2.0f, -1.5f,  2.0f,
+            2.0f, -1.5f,  2.0f,
+            -2.0f,  1.5f,  2.0f,
+            2.0f,  1.5f,  2.0f,
+            // BACK
+            -2.0f, -1.5f, -2.0f,
+            -2.0f,  1.5f, -2.0f,
+            2.0f, -1.5f, -2.0f,
+            2.0f,  1.5f, -2.0f,
+            // LEFT
+            -2.0f, -1.5f,  2.0f,
+            -2.0f,  1.5f,  2.0f,
+            -2.0f, -1.5f, -2.0f,
+            -2.0f,  1.5f, -2.0f,
+            // RIGHT
+            2.0f, -1.5f, -2.0f,
+            2.0f,  1.5f, -2.0f,
+            2.0f, -1.5f,  2.0f,
+            2.0f,  1.5f,  2.0f,
+            // TOP
+            -2.0f,  1.5f,  2.0f,
+            2.0f,  1.5f,  2.0f,
+            -2.0f,  1.5f, -2.0f,
+            2.0f,  1.5f, -2.0f,
+            // BOTTOM
+            -2.0f, -1.5f,  2.0f,
+            -2.0f, -1.5f, -2.0f,
+            2.0f, -1.5f,  2.0f,
+            2.0f, -1.5f, -2.0f,
+    };
+    final static float camTexCoords[] = new float[] {
+            // Camera preview
+            0.0f, 0.0f,
+            0.9375f, 0.0f,
+            0.0f, 0.625f,
+            0.9375f, 0.625f,
+
+            // BACK
+            0.9375f, 0.0f,
+            0.9375f, 0.625f,
+            0.0f, 0.0f,
+            0.0f, 0.625f,
+            // LEFT
+            0.9375f, 0.0f,
+            0.9375f, 0.625f,
+            0.0f, 0.0f,
+            0.0f, 0.625f,
+            // RIGHT
+            0.9375f, 0.0f,
+            0.9375f, 0.625f,
+            0.0f, 0.0f,
+            0.0f, 0.625f,
+            // TOP
+            0.0f, 0.0f,
+            0.9375f, 0.0f,
+            0.0f, 0.625f,
+            0.9375f, 0.625f,
+            // BOTTOM
+            0.9375f, 0.0f,
+            0.9375f, 0.625f,
+            0.0f, 0.0f,
+            0.0f, 0.625f
+    };
+
 }
